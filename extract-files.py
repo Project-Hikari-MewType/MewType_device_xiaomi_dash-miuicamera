b129125dd926d7a9410e38499489b14336dbaa8a
therealmharc <therealmharc@gmail.com>
2026-09-12 18:26:22 +0700

    dash-miuicamera: add more patch and sepolicy
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>
    Co-authored-by: Hexaboo <hexaboo@luminedroid.org>
    Co-authored-by: Hikari-Soni <247soni01@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -33,8 +33,12 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+      'system_ext/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so')
+        .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
+      'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
+         .replace_needed('android.hardware.graphics.common-V6-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     (
-        'system_ext/lib64/libcamera_algoup_jni.xiaomi.so',
         'system_ext/lib64/libcamera_mianode_jni.xiaomi.so',
         'system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so'
     ): blob_fixup()
